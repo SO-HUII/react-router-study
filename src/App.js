c0287f9f8510d66fa8c0bf06b4721ddaf28fdfd1
SO HUI <haejuuun925@gmail.com>
2024-01-09 21:44:38 +0900

    feat: jsx 방식으로 route 정의

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
 
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
   { path: '/products', element: <ProductsPage /> },
 ]);
+
+// const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
