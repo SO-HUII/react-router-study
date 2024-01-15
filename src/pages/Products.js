fff31177ec5f61d82c7bdcbd2e8106df62fc9d5d
SO HUI <haejuuun925@gmail.com>
2024-01-15 21:55:15 +0900

    feat: 동적 라우트에 링크 추가 (useParams 이용)

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+const PRODUCTS = [
+  { id: 'p1', title: 'Product1' },
+  { id: 'p2', title: 'Product2' },
+  { id: 'p3', title: 'Product3' }
+];
 
 function ProductsPage() {
   return (
-    <h1>Products Page</h1>
+    <>
+      <h1>Products Page</h1>
+      <ul>
+        {PRODUCTS.map((prod) => (
+          <li key={prod.id}><Link></Link>
+            <Link to={`/products/${prod.id}`}>{prod.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
